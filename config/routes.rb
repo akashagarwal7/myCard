16b91be740b92479ceac7e0cd5e2956908bb5532
Akash Agarwal <akashaggarwal777@gmail.com>
2018-09-09 12:06:25 +1000

    FIX: Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'receiver/home'
+
   resources :data_types, path: 'types'
   resources :mykis
   resources :passports
@@ -6,5 +8,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
 
-  get 'api/id_info', to: 'apis#info'
+  get 'info', to: 'apis#info'
+
+  get 'receiver/home', to: 'receiver#home'
+
 end
