34e1ac2e98615e8590fe8e5f1b0b7e851debc996
Akash Agarwal <akashaggarwal777@gmail.com>
2018-09-09 16:16:16 +1000

    FIX: Routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :users
 
   get 'info', to: 'apis#info'
-  root 'receiver/home', to: 'receiver#home'
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'receiver/home', to: 'receiver#home'
+
+  root 'receiver#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
